fa86a8884dca6f9d4911482aa93fde65b9f68ed9
sabrina-mei <slmei2@illinois.edu>
2025-06-18 14:10:32 -0500

    completed amino acid count and frequency

--- codon_analysis.py
+++ codon_analysis.py
@@ -29,7 +29,7 @@ for key, val in codonTable.items():
 
 # type of sequence is either 'rna' or 'dna'
 # return tuple (dictionary, int): ({key (codon), value (count)}, total num of codons)
-def analyze(sequence: str, type: str):
+def analyzeCodons(sequence: str, type: str):
     if type.lower() == 'dna':
         sequence = sequence.replace('T', 'U')
     
@@ -37,13 +37,20 @@ def analyze(sequence: str, type: str):
     if idx < 0:
         raise ValueError('Sequence does not contain a start codon')
     
-    freq = {key: 0 for key in codonTable}
+    count = {key: 0 for key in codonTable}
     total = 0
     while idx < len(sequence) - 2:
         codon = sequence[idx:idx+3]
-        freq[codon] += 1
+        count[codon] += 1
 
         total += 1
         idx += 3
 
-    return (freq, idx)
+    return (count, total)
+
+def analyzeAminoAcids(codons: dict):
+    count = {key: 0 for key in revCodonTable}
+    for key, val in revCodonTable.items():
+        for c in val:
+            count[key] += codons[c]
+    return count
--- main.py
+++ main.py
@@ -16,21 +16,61 @@ with open(file_name, 'r') as fa:
         names.append(record.id)
         seqs.append(str(record.seq))
 
-# plotting codon distribution
-data = codon_analysis.analyze(seqs[0], 'dna')[0]
+# computing codon count and frequencies
+rawData = codon_analysis.analyzeCodons(seqs[0], 'dna')
+data = rawData[0]
 sorted_data = sorted(data.items(), key=lambda item: item[1], reverse=True)
-xVals, yVals = zip(*sorted_data)
+codons, codonCount = zip(*sorted_data)
+codonFreq = tuple(x / rawData[1] for x in codonCount)
 
-plt.figure(figsize=(12, 6))
-plt.bar(xVals, yVals, color='turquoise')
-plt.margins(x=0.01)
+# plotting codon distribution (count and frequency)
+fig, ax1 = plt.subplots(figsize=(12, 6))
+fig.suptitle('Codon Count and Frequency', fontsize=14)
 
-plt.xlabel('Codon')
-plt.xticks(fontsize=9, rotation=45, ha='right')
-plt.ylabel('Count')
-plt.title('Count of Codons in Sequence')
+# Bar plot for count
+bars = ax1.bar(codons, codonCount, color='turquoise', label='Count')
+ax1.set_xlabel('Codon')
+ax1.set_ylabel('Count')
+ax1.margins(x=0.01)
+
+# Second y-axis for frequency
+ax2 = ax1.twinx()
+ax2.plot(codons, codonFreq, alpha=0) # transparent bc don't want it to shown on the plot, looks identical to count
+ax2.set_ylabel('Frequency (Fraction)')
+ax2.margins(x=0.01)
+
+# X-ticks styling
+ax1.set_xticks(range(len(codons)))
+ax1.set_xticklabels(codons, fontsize=9, rotation=45, ha='right')
+
+plt.tight_layout()
+
+# computing amino acid count and frequencies
+aa = codon_analysis.analyzeAminoAcids(data)
+print(aa)
+sorted_aa = sorted(aa.items(), key=lambda item: item[1], reverse=True)
+codo, aaCount = zip(*sorted_aa)
+aaFreq = tuple(x / rawData[1] for x in aaCount)
+
+# plotting codon distribution (count and frequency)
+fig, ax1 = plt.subplots(figsize=(9, 6))
+fig.suptitle('Amino Acid Count and Frequency', fontsize=14)
+
+# Bar plot for count
+bars = ax1.bar(codo, aaCount, color='turquoise', label='Count')
+ax1.set_xlabel('Codon')
+ax1.set_ylabel('Count')
+ax1.margins(x=0.01)
+
+# Second y-axis for frequency
+ax2 = ax1.twinx()
+ax2.plot(codo, aaFreq, alpha=0) # transparent bc don't want it to shown on the plot, looks identical to count
+ax2.set_ylabel('Frequency')
+ax2.margins(x=0.01)
+
+# X-ticks styling
+ax1.set_xticks(range(len(codo)))
+ax1.set_xticklabels(codo, fontsize=9, rotation=45, ha='right')
 
 plt.tight_layout()
 plt.show()
-
-
