69ae0e83b51d301d5d9977415819c8e642e2b0d0
sabrina-mei <slmei2@illinois.edu>
2025-06-26 11:41:13 -0500

    updated README

--- README.md
+++ README.md
@@ -1,34 +1,43 @@
 # Codon Usage Analysis
 
-Given a DNA or RNA sequence, various codon usage statistics are calculated and visualized in graphs. This includes the following:
-- Compute and plot count and frequencies of each codon in the sequence
-- Compute and plot count and frequencies of each amino acid in the sequence
-  - Compute count and frequencies of each codon for each amino acid
-      - For example: UUU and UUC code for phenylalanine (F), UUUUUCUUU would have 3 F's be 100% F, but of that, 2/3 are UUU and 1/3 are UUC
-- Compute and plot Relative Synonymous Codon Usage (RSCU) scores
-  - Create a heatmap of RSCU scores to compare between different sequences
+Given a single DNA or RNA sequence, various codon usage statistics are calculated and visualized in graphs. This includes the following:
+- Compute and plot the count and frequencies of each codon in the sequence
+- Compute and plot the count and frequencies of each amino acid in the sequence
+- Compute and plot Relative Synonymous Codon Usage (RSCU) scores for each codon
 - Compute GC content in the sequence
   - Compute GC content of codon positions 1 and 2 compared to position 3
   - Graph GC content distribution across the sequence
-        
+ 
+Given multiple sequences, various comparisons are calculated and visualized in graphs:
+- Create a heatmap of RSCU scores (codons vs sequences, values are RSCU scores)
+- Compute and plot the Effective Number of Codons (ENC) for each sequence
+- Create a scatterplot of ENC vs GC3 (GC content of codon position 3), each data point is a different sequence
+
+## Features and Funcions
+
+- Codon and amino acid counts: `analysis.analyze_codons()`, `analysis.analyze_amino_acids()`, `plotting.bar_count_freq()`
+- RSCU calculation and heatmap: `analysis.rscu()`, `plotting.rscu`, `plotting.rscu_heatmap()`
+- GC content analysis: `analysis.gc()`, `plotting.gc()`
+- ENC calculation and plots: `analysis.enc()`, `plotting.enc()`, `plotting.enc_vs_gc3()`
 
 ## Getting Started
 
 ### Prerequisites
 - Python 3.12.5 or newer
-- Python libraries: `matplotlib`, `os`, `Bio`, `pandas`, `seaborn`
 
 ### Installing
-
-
-
+Install the required Python libraries:
+```bash
+pip install matplotlib biopython pandas seaborn
+```
+Run sample data that generates sample plots:
+```bash
+python main.py
+```
 ## Authors
 
   - Sabrina Mei
 
-## License
 
 
-
-## Acknowledgments
 
---
+++ single_seq_example.py
@@ -0,0 +1,72 @@
+import os
+from Bio import SeqIO
+import analysis
+import plotting
+
+# read file
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_name = os.path.join(script_dir, 'raw_data/test_single.fasta')
+
+# open file as fasta (each line is either the name of a sequence or a sequence itself)
+names = []
+seqs = []
+with open(file_name, 'r') as fa:
+    for record in SeqIO.parse(fa, 'fasta'):
+        names.append(record.id)
+        seqs.append(str(record.seq))
+
+# directories to save things to
+plot_output_dir = 'plots'
+if not os.path.exists(plot_output_dir):
+    os.makedirs(plot_output_dir)
+
+data_output_dir = 'output'
+if not os.path.exists(data_output_dir):
+    os.makedirs(data_output_dir)
+
+# every function is tested using first sequence in the fasta file
+# computing codon count and frequencies
+gene_name = names[0]
+data = analysis.analyze_codons(seqs[0])
+
+# Generate codon usage bar graph
+# Create dynamic filename to avoid overwriting
+safe_name = gene_name.replace(':', '_') # can't have : in file names
+output_filename = os.path.join(plot_output_dir, f'{safe_name}_codon_usage.png')
+plotting.bar_count_freq(data, "Codon Count and Frequency", "Codon", output_filename)
+
+# computing amino acid count and frequencies and plotting
+aa_data = analysis.analyze_amino_acids(data)
+output_filename = os.path.join(plot_output_dir, f'{safe_name}_amino_acid_usage.png')
+plotting.bar_count_freq(aa_data, "Amino Acid Count and Frequency", "Amino Acid", output_filename)
+
+# calculating Relative Synonymous Codon Usage and plotting
+rscu_data = analysis.rscu(data, aa_data)
+output_filename = os.path.join(plot_output_dir, f'{safe_name}_rscu.png')
+plotting.rscu(rscu_data, output_filename)
+
+# write RSCU output to file
+output_filename = os.path.join(data_output_dir, f'{safe_name}_rscu.txt')
+with open(output_filename, 'w') as file:
+    for key, val in rscu_data.items():
+        file.write(key + ': ' + str(val) + '\n')
+
+# calculating gc content and plotting it
+output_filename = os.path.join(plot_output_dir, f'{safe_name}_gc.png')
+output2_filename = os.path.join(plot_output_dir, f'{safe_name}_gc_bp.png')
+plotting.gc(seqs[0], 30, output_filename, output2_filename)
+
+# rscu heatmap
+heat_name = 'GAPDH'
+output_filename = os.path.join(plot_output_dir, f'{heat_name}_codon_usage_heatmap.png')
+species = ['Homo\nsapiens', 'Drosophila\nmelanogaster', 'Saccharomyces\ncerevisiae', 'Escherichia\ncoli'] # row labels
+plotting.rscu_heatmap(species, seqs, 'GAPDH RSCU Across Different Species', output_filename)
+
+# calculate and plot all enc values
+output_filename = os.path.join(plot_output_dir, 'ENC_values.png')
+plotting.enc(species, seqs, 'GAPDH', output_filename)
+
+# enc vs gc3
+output_filename = os.path.join(plot_output_dir, 'ENC_vs_GC3.png')
+plotting.enc_vs_gc3(species, seqs, 'GAPDH', output_filename)
+
